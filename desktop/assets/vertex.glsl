358c11283942e88b3317522387d2cf2d21e14111
JulianSobott <julian.sobott@gmail.com>
2018-04-04 23:01:53 +0200

    TRied to add some wave effects

--- desktop/assets/vertex.glsl
+++ desktop/assets/vertex.glsl
@@ -4,11 +4,17 @@ attribute vec4 a_color;
 
 //our camera matrix
 uniform mat4 u_projTrans;
+//uniform mat4 u_rotationMatrix;
+//uniform mat4 u_moveMatrix;
 
 //send the color out to the fragment shader
 varying vec4 vColor;
 
 void main() {
-    vColor = a_color;
-    gl_Position = u_projTrans * vec4(a_position.xyz, 1.0f);
+    float distanceFactor = distance(a_position.x, 100)/(100.0f*sqrt(2));
+    vec3 color = vec3(distanceFactor*.1f,distanceFactor* .1f, 255.0f);
+	vColor = vec4(color, 1.0f);
+    //gl_Position = u_moveMatrix * u_rotationMatrix * u_projTrans * vec4(a_position.xyz, 1.0f);
+gl_Position = u_projTrans * vec4(a_position.xyz, 1.0f);
+
 }
