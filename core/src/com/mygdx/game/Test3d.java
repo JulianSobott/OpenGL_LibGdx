f83a1c24546e57869a268e6ed852aa97fc1e576d
JulianSobott <julian.sobott@gmail.com>
2018-04-26 18:06:33 +0200

    added all Normals

--- core/src/com/mygdx/game/Test3d.java
+++ core/src/com/mygdx/game/Test3d.java
@@ -1,5 +1,8 @@
 package com.mygdx.game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
@@ -55,7 +58,7 @@ public class Test3d implements ApplicationListener {
 		
 		modelBatch = new ModelBatch();
 		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		cam.position.set(0, 0, 10f);
+		cam.position.set(0, 0, 20f);
 		cam.lookAt(0, 0, 0);
 		cam.near = 1f;
 		cam.far = 300f;
@@ -70,42 +73,17 @@ public class Test3d implements ApplicationListener {
 		//create water mesh
 		float[] vertices = createGridVertices();
 		short[] indices = createGridIndices(vertices);
-		waterMesh = new Mesh(false, NUM_ROWS*NUM_COLS*3, numTris*3, new VertexAttribute(VertexAttributes.Usage.Position, 3, ShaderProgram.POSITION_ATTRIBUTE));
+		createNormals(indices, vertices);
+		waterMesh = new Mesh(false, NUM_ROWS*NUM_COLS*3, numTris*3, new VertexAttributes(new VertexAttribute(VertexAttributes.Usage.Position, 3, ShaderProgram.POSITION_ATTRIBUTE), new VertexAttribute(Usage.Normal,  3, ShaderProgram.NORMAL_ATTRIBUTE)));
 		waterMesh.setIndices(indices);
 		waterMesh.setVertices(vertices);
-		//MeshBuilder meshBuilder = new MeshBuilder();
-		//meshBuilder.begin(Usage.Position, GL30.GL_TRIANGLES);
-		//meshBuilder.addMesh(waterMesh);
-		//waterMesh = meshBuilder.end();
+		
 		ModelBuilder modelBuilder = new ModelBuilder();
 		modelBuilder.begin();
 		modelBuilder.part("water", waterMesh, GL30.GL_TRIANGLES, new Material(ColorAttribute.createDiffuse(Color.RED)));
 		model = modelBuilder.end();
 		//model = modelBuilder.createBox(5, 5, 5, new Material(ColorAttribute.createDiffuse(Color.RED)), Usage.Position | Usage.Normal);
-		instance = new ModelInstance(model);
-		//instance.transform.translate(new Vector3(-5, -5, 0));
-		//instance.transform.rotate(Vector3.Y, 30);
-		/*model = modelBuilder.createRect(10, 10, 0, 0, 0, 10, 0, 10, 0, 0, 10, 10, 0, .5f, .5f, new Material(ColorAttribute.createDiffuse(Color.GREEN)), Usage.Position | Usage.Normal);
-		instance = new ModelInstance(model);
-		//================================================================
-		/*MeshBuilder meshBuilder = new MeshBuilder();
-		meshBuilder.begin(Usage.Position | Usage.Normal, GL30.GL_TRIANGLES);
-		meshBuilder.rect(new Vector3(0, 0, 0), new Vector3(10, 0, 0), new Vector3(0, 10, 0), new Vector3(10, 10, 0), new Vector3(0, 0, 1));
-		Mesh side1 = meshBuilder.end();
-		
-		meshBuilder.begin(new VertexAttributes(VertexAttribute.Position(), VertexAttribute.Normal()));
-		meshBuilder.rect(new Vector3(0, 0, 0), new Vector3(0, 0, 10), new Vector3(0, 10, 0), new Vector3(0, 10, 10), new Vector3(1, 0, 0));
-		Mesh side2 = meshBuilder.end();
-		
-		//Model side1 = modelBuilder.createRect(0, 0, 0, 10, 0, 0, 10, 10, 0, 0, 10, 0, 0, 0, 1, new Material(ColorAttribute.createDiffuse(Color.GREEN)), Usage.Position | Usage.Normal);
-		Model side2 = modelBuilder.createRect(0, 0, 0, 10, 0, 10, 10, 10, 10, 0, 10, 10, 0, 0, 1, new Material(ColorAttribute.createDiffuse(Color.GREEN)), Usage.Position | Usage.Normal);
-		Model side3 = modelBuilder.createLineGrid(5, 5, 5, 5,  new Material(ColorAttribute.createDiffuse(Color.GREEN)), Usage.Position);
-		instance = new ModelInstance(side3);
-		modelBuilder.begin();
-		//modelBuilder.part(side1.meshParts.get(0), side1.materials.get(0));
-		modelBuilder.part(side2.meshParts.get(0), side2.materials.get(0));
-		model = modelBuilder.end();*/
-	//	instance = new ModelInstance(model);			
+		instance = new ModelInstance(model);			
 	}
 
 	@Override
@@ -177,22 +155,132 @@ public class Test3d implements ApplicationListener {
 	
 	private float[] createGridVertices() {
 		
-		float[] vertices = new float[NUM_ROWS * NUM_COLS * 3];
+		float[] vertices = new float[NUM_ROWS * NUM_COLS * 3 * 2]; //*2 for normals
 		int i = 0;
 		float xStart = -Gdx.graphics.getWidth()/2;
-		xStart = -10;
+		xStart = -20;
 		float yStart = -Gdx.graphics.getHeight()/2;
-		yStart = -10;
+		yStart = -20;
 		for(int row = 0; row< NUM_COLS; row++) {
 			for(int col = 0; col < NUM_ROWS; col++) {
 				int coordinate = 0; 
-				vertices[i + coordinate++] = xStart + TRI_WIDTH * col; //x
-				vertices[i + coordinate++] = yStart + TRI_WIDTH * row; //x
-				vertices[i + coordinate++] = (float) (Math.random()*3-3); //x
+				float x = xStart + TRI_WIDTH * col;
+				float y = yStart + TRI_WIDTH * row;
+				float z = (float) (Math.random()*3-3);
+				vertices[i + coordinate++] = x;
+				vertices[i + coordinate++] = y;
+				vertices[i + coordinate++] = z;
+				//skip for normals
+				coordinate += 3;
 				i += coordinate;
 			}
 		}
 		return vertices;
 	}
 
+	private void createNormals(short[] indices, float[] vertices) {
+		//calculate face normals
+		Vector3[] faceNormals = new Vector3[numTris];
+		int face = 0;
+		Vector3 orientationPoint = new Vector3(0, 0, 40);
+		for(int row = 0; row < NUM_ROWS-1; row++) {
+			for(int col = 0; col < NUM_COLS-1; col++) { 
+				int leftBot = row * NUM_COLS + col;
+				int leftTop = row * NUM_COLS + col + NUM_COLS;
+				int rightBot = row * NUM_COLS + col + 1;
+				int rightTop = row * NUM_COLS + col + NUM_COLS + 1;
+				
+				Vector3 vec1 = getVertexPosition(leftTop, vertices).sub(getVertexPosition(leftBot, vertices));
+				Vector3 vec2 = getVertexPosition(rightBot , vertices).sub(getVertexPosition(leftBot, vertices));
+				faceNormals[face++] = vec1.crs(vec2);
+				faceNormals[face -1].nor();
+				
+				if(faceNormals[face-1].dot(new Vector3(orientationPoint.sub(getVertexPosition(leftBot, vertices)))) < 0) {
+					faceNormals[face-1].scl(-1, -1, -1);
+				}
+				
+				vec1 = getVertexPosition(leftTop, vertices).sub(getVertexPosition(rightTop, vertices));
+				vec2 = getVertexPosition(rightBot, vertices).sub(getVertexPosition(rightTop, vertices));
+				faceNormals[face++] = vec2.crs(vec1);
+				faceNormals[face -1].nor();
+				
+				if(faceNormals[face-1].dot(new Vector3(orientationPoint.sub(getVertexPosition(rightTop, vertices)))) < 0) {
+					faceNormals[face-1].scl(-1, -1, -1);
+				}
+			}
+		}
+		//calculate vertex normals
+		Vector3 vertexNormal = new Vector3();
+		int innerVert = 0;
+		int bottomVert = 0;
+		for(int idxPosition = 0; idxPosition < vertices.length/6; idxPosition++) {
+			//calculate surrounding faces
+
+			List<Integer> surroundingFaces  = new ArrayList<Integer>();
+			//checking edges
+			if((float)idxPosition / NUM_COLS < 1 || idxPosition == 0) { //Bottom edge
+				if((float)idxPosition % NUM_COLS == 0) { //bottom Left corner
+					surroundingFaces.add(0);
+				}else if(((float)idxPosition+1)% NUM_COLS == 0) {//bottom right corner
+					surroundingFaces.add(idxPosition - 1 + bottomVert);
+					surroundingFaces.add(idxPosition + bottomVert);
+				}else {
+					surroundingFaces.add(idxPosition - 1 + bottomVert);
+					surroundingFaces.add(idxPosition + bottomVert);
+					surroundingFaces.add(idxPosition + 1 + bottomVert);
+					bottomVert++;
+				}
+			}else if((float)idxPosition % NUM_COLS == 0 && idxPosition != 0 && NUM_COLS * NUM_ROWS - idxPosition > NUM_COLS) { //Left edge
+				surroundingFaces.add((idxPosition/NUM_COLS - 1) * (NUM_COLS-1)*2);
+				surroundingFaces.add((idxPosition/NUM_COLS - 1) * (NUM_COLS-1)*2 + 1);
+				surroundingFaces.add((idxPosition/NUM_COLS) * (NUM_COLS-1)*2);
+			}else if(((float)idxPosition+1)% NUM_COLS == 0 && NUM_COLS * NUM_ROWS - idxPosition > NUM_COLS) { //right edge
+				surroundingFaces.add(((idxPosition+1)/NUM_COLS - 1) * (NUM_COLS-1)*2 - 1);				
+				surroundingFaces.add(((idxPosition+1)/NUM_COLS - 1) * (NUM_COLS-1)*2 - 1 + (NUM_COLS-1)*2-1);
+				surroundingFaces.add(((idxPosition+1)/NUM_COLS - 1) * (NUM_COLS-1)*2 + (NUM_COLS-1)*2-1);
+			}else if(NUM_COLS * NUM_ROWS - idxPosition <= NUM_COLS) { //top edge
+				if((float)idxPosition % NUM_COLS == 0) { //Top left corner
+					surroundingFaces.add((idxPosition/NUM_COLS - 1) * (NUM_COLS-1)*2);
+					surroundingFaces.add((idxPosition/NUM_COLS - 1) * (NUM_COLS-1)*2 + 1);
+				}else if(((float)idxPosition+1)% NUM_COLS == 0) { //top right corner
+					surroundingFaces.add(((idxPosition+1)/NUM_COLS - 1) * (NUM_COLS-1)*2 - 1);
+				}else {
+					//TODO add this INDEZEs
+					int row = (int) idxPosition / NUM_COLS;
+					int col = idxPosition - row * NUM_COLS;
+					surroundingFaces.add((row-1) * (NUM_COLS-1)*2 + col);
+					surroundingFaces.add((row-1) * (NUM_COLS-1)*2 + col + 1);
+					surroundingFaces.add((row-1) * (NUM_COLS-1)*2 + col + 2);
+				}
+			}else {
+				//non edges
+				surroundingFaces.add(idxPosition - NUM_COLS + innerVert);
+				surroundingFaces.add(idxPosition - NUM_COLS + innerVert + 1);
+				surroundingFaces.add(idxPosition - NUM_COLS + innerVert + 2);
+				
+				surroundingFaces.add(idxPosition - NUM_COLS - 1 + (NUM_COLS-1)*2 + innerVert);
+				surroundingFaces.add(idxPosition - NUM_COLS - 1 + (NUM_COLS-1)*2 + innerVert + 1);
+				surroundingFaces.add(idxPosition - NUM_COLS - 1 + (NUM_COLS-1)*2 + innerVert + 2);
+				innerVert++;
+			}
+			
+			
+			for(int idxFace : surroundingFaces) {
+				vertexNormal.add(faceNormals[idxFace]);
+			}
+			vertexNormal.nor();
+			
+			vertices[idxPosition * 6 + 3] = vertexNormal.x;
+			vertices[idxPosition * 6 + 1 + 3] = vertexNormal.y;
+			vertices[idxPosition * 6 + 2 + 3] = vertexNormal.z;
+		}
+		System.out.println();
+	}
+	
+	private Vector3 getVertexPosition(int index, float[] vertices) {
+		float x = vertices[index * 6]; //6 for 3 position and 3 normals
+		float y = vertices[index * 6 + 1];
+		float z = vertices[index * 6 + 2];
+		return new Vector3(x, y, z);
+	}
 }
