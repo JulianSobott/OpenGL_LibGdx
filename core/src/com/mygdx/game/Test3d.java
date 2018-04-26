8c55d939d4266475afd5619cf1f89e74920bd467
JulianSobott <julian.sobott@gmail.com>
2018-04-26 11:27:29 +0200

    perspective WaterGrid using z-index

--- core/src/com/mygdx/game/Test3d.java
+++ core/src/com/mygdx/game/Test3d.java
@@ -26,6 +26,7 @@ import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.MeshBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Matrix4;
@@ -38,15 +39,23 @@ public class Test3d implements ApplicationListener {
 	private Model model;
 	private ModelInstance instance;
 	private Environment environment;
+	PointLight pointLight;
 	private CameraInputController camController;
 	
-	private ShapeRenderer sr;
+	private Mesh waterMesh;
+	
+	final int NUM_ROWS = 10;
+	final int NUM_COLS = 10;
+	final int TRI_WIDTH = 2;
+	int numTris;
 	
 	@Override
 	public void create() {
+		numTris = (NUM_COLS-1) * (NUM_ROWS-1);
+		
 		modelBatch = new ModelBatch();
 		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		cam.position.set(0, 0, 20f);
+		cam.position.set(0, 0, 10f);
 		cam.lookAt(0, 0, 0);
 		cam.near = 1f;
 		cam.far = 300f;
@@ -55,11 +64,24 @@ public class Test3d implements ApplicationListener {
 		Gdx.input.setInputProcessor(camController);
 
 		environment = new Environment();
-		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, .1f, .1f, .1f, 1f));
-		environment.add(new PointLight().set(0.8f, 0.8f, 0.8f, 5f, 2f, 2f, 20f));
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, .4f, .4f, .4f, 1f));
+		environment.add(pointLight = new PointLight().set(0.8f, 0.8f, 0.8f, 0f, 20f, 5f, 2f));
+		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+		//create water mesh
+		float[] vertices = createGridVertices();
+		short[] indices = createGridIndices(vertices);
+		waterMesh = new Mesh(false, NUM_ROWS*NUM_COLS*3, numTris*3, new VertexAttribute(VertexAttributes.Usage.Position, 3, ShaderProgram.POSITION_ATTRIBUTE));
+		waterMesh.setIndices(indices);
+		waterMesh.setVertices(vertices);
+		//MeshBuilder meshBuilder = new MeshBuilder();
+		//meshBuilder.begin(Usage.Position, GL30.GL_TRIANGLES);
+		//meshBuilder.addMesh(waterMesh);
+		//waterMesh = meshBuilder.end();
 		ModelBuilder modelBuilder = new ModelBuilder();
-		model = modelBuilder.createRect(0, 0, 0, 5, 0, 0, 5, 5, 0, 0, 5, 0, 0, 1, 1, new Material(ColorAttribute.createDiffuse(Color.GREEN)), Usage.Position | Usage.Normal);
-		//model = modelBuilder.createSphere(1f, 1f, 1f, 20, 10, new Material(ColorAttribute.createDiffuse(Color.GREEN)), Usage.Position | Usage.Normal);
+		modelBuilder.begin();
+		modelBuilder.part("water", waterMesh, GL30.GL_TRIANGLES, new Material(ColorAttribute.createDiffuse(Color.RED)));
+		model = modelBuilder.end();
+		//model = modelBuilder.createBox(5, 5, 5, new Material(ColorAttribute.createDiffuse(Color.RED)), Usage.Position | Usage.Normal);
 		instance = new ModelInstance(model);
 		//instance.transform.translate(new Vector3(-5, -5, 0));
 		//instance.transform.rotate(Vector3.Y, 30);
@@ -91,9 +113,11 @@ public class Test3d implements ApplicationListener {
 		// TODO Auto-generated method stub
 		
 	}
-
+	int tick = 0;
 	@Override
 	public void render() {
+		tick++;
+		pointLight.setPosition((float)(10*Math.sin(tick)), 10, 20);
 		camController.update();
 		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT | GL30.GL_DEPTH_BUFFER_BIT);
@@ -121,5 +145,44 @@ public class Test3d implements ApplicationListener {
 		model.dispose();
 	}
 	
+	private short[] createGridIndices(float[] vertices) {
+		short[] indices = new short[numTris*3];
+		int idxVertex = 0;
+		for(int tri = 0; tri < numTris*3 ; tri++) {
+			if((idxVertex+1) % NUM_COLS != 0 || idxVertex == 0) {
+				int i = 0;
+				indices[tri + i++] = (short) idxVertex;
+				indices[tri + i++] = (short) (idxVertex + 1);
+				indices[tri + i++] = (short) (idxVertex + NUM_COLS);
+				idxVertex++;
+				tri+=2;
+			}else {
+				idxVertex++;
+				tri--;
+			}
+			
+		}
+		return indices;
+	}
+	
+	private float[] createGridVertices() {
+		
+		float[] vertices = new float[NUM_ROWS * NUM_COLS * 3];
+		int i = 0;
+		float xStart = -Gdx.graphics.getWidth()/2;
+		xStart = -20;
+		float yStart = -Gdx.graphics.getHeight()/2;
+		yStart = -20;
+		for(int row = 0; row< NUM_COLS; row++) {
+			for(int col = 0; col < NUM_ROWS; col++) {
+				int coordinate = 0; 
+				vertices[i + coordinate++] = xStart + TRI_WIDTH * col; //x
+				vertices[i + coordinate++] = yStart + TRI_WIDTH * row; //x
+				vertices[i + coordinate++] = (float) (Math.random()*3-3); //x
+				i += coordinate;
+			}
+		}
+		return vertices;
+	}
 
 }
